cde7c3fb8b8f6a91514a662c15b5f58e29d1a759
NinoSlaats <nino.slaats@student.gildeopleidingen.nl>
2026-06-17 09:00:06 +0200

    new

--- index.php
+++ index.php
@@ -49,3 +49,5 @@ if (!isset($_SESSION['user_id'])) {
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+// ngrok http 80
+// http://localhost/crm/login.php
